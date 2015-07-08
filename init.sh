a1e24a5cac9cc16f5828ddb1fc917cd844483486
Michael J. Stealey <Michael.J.Stealey@gmail.com>
2015-07-08 11:04:58 -0400

    update for use on ginotebook.renci.org

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sleep 3s
 psql -U postgres -h postgis -w -c 'create extension postgis;'
 echo "made sure postgis was enabled on PostgreSQL"
 /usr/sbin/sshd
-python manage.py runserver 0.0.0.0:9000
+python manage.py runserver 0.0.0.0:8000
