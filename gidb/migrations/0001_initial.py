93a3db593b537af647859d92f1a99b79ac7544c3
Michael J. Stealey <Michael.J.Stealey@gmail.com>
2015-07-22 13:58:43 -0400

    update migrations

--- gidb/migrations/0001_initial.py
+++ gidb/migrations/0001_initial.py
@@ -17,6 +17,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=64)),
             ],
+            options={
+                'verbose_name': 'Green infrastructure element',
+            },
         ),
         migrations.CreateModel(
             name='GIInstance',
@@ -24,6 +27,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('placement_poly', django.contrib.gis.db.models.fields.PolygonField(srid=4326)),
             ],
+            options={
+                'verbose_name': 'Green infrastructure instance',
+            },
         ),
         migrations.CreateModel(
             name='GITemplate',
@@ -32,6 +38,9 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(unique=True, max_length=64)),
                 ('gi_elements', models.ManyToManyField(to='gidb.GIElement')),
             ],
+            options={
+                'verbose_name': 'Green infrastructure template',
+            },
         ),
         migrations.CreateModel(
             name='GIVegGrowthState',
@@ -43,38 +52,67 @@ class Migration(migrations.Migration):
                 ('gi_element', models.ForeignKey(to='gidb.GIElement')),
                 ('gi_instance', models.ForeignKey(to='gidb.GIInstance')),
             ],
+            options={
+                'verbose_name': 'Green infrastructure growth state',
+            },
         ),
         migrations.CreateModel(
             name='HumanPrefImage',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=64)),
                 ('pref_image', models.FileField(upload_to=b'')),
                 ('gi_instance', models.ForeignKey(to='gidb.GIInstance')),
             ],
+            options={
+                'verbose_name': 'Human preference image',
+            },
         ),
         migrations.CreateModel(
-            name='Representation',
+            name='Representation2D',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=64)),
+                ('rep_file', models.FileField(upload_to=b'')),
+                ('rep_thumbnail', models.FileField(null=True, upload_to=b'', blank=True)),
+            ],
+            options={
+                'verbose_name': '2D visual representation',
+            },
+        ),
+        migrations.CreateModel(
+            name='Representation3D',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=64)),
                 ('rep_file', models.FileField(upload_to=b'')),
                 ('rep_thumbnail', models.FileField(upload_to=b'')),
             ],
+            options={
+                'verbose_name': '3D visual representation',
+            },
         ),
         migrations.CreateModel(
-            name='RHESSSysSoilType',
+            name='RHESSysSoilType',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(unique=True, max_length=128)),
-                ('rhessys_default_id', models.IntegerField(unique=True)),
+                ('name', models.CharField(unique=True, max_length=64)),
+                ('rhessys_default_id', models.CharField(unique=True, max_length=8)),
             ],
+            options={
+                'verbose_name': 'RHESSys soil type',
+            },
         ),
         migrations.CreateModel(
             name='RHESSysStratumType',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(unique=True, max_length=128)),
-                ('rhessys_default_id', models.IntegerField(unique=True)),
+                ('name', models.CharField(unique=True, max_length=64)),
+                ('rhessys_default_id', models.CharField(unique=True, max_length=8)),
             ],
+            options={
+                'verbose_name': 'RHESSys stratum type',
+            },
         ),
         migrations.CreateModel(
             name='TemplatesForEcoClimate',
@@ -83,29 +121,31 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(unique=True, max_length=64)),
                 ('templates', models.ManyToManyField(to='gidb.GITemplate')),
             ],
+            options={
+                'verbose_name': 'Eco-climate GI template',
+            },
         ),
         migrations.CreateModel(
             name='Watershed',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=64)),
-                ('model_url', models.URLField(max_length=512)),
+                ('model_url', models.URLField(max_length=1024)),
                 ('template_menu', models.ForeignKey(to='gidb.TemplatesForEcoClimate')),
             ],
+            options={
+                'verbose_name': 'Watershed',
+            },
         ),
-        migrations.CreateModel(
-            name='Representation2D',
-            fields=[
-                ('representation_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='gidb.Representation')),
-            ],
-            bases=('gidb.representation',),
+        migrations.AddField(
+            model_name='gitemplate',
+            name='model_3d',
+            field=models.ForeignKey(to='gidb.Representation3D'),
         ),
-        migrations.CreateModel(
-            name='Representation3D',
-            fields=[
-                ('representation_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='gidb.Representation')),
-            ],
-            bases=('gidb.representation',),
+        migrations.AddField(
+            model_name='gitemplate',
+            name='model_planview',
+            field=models.ForeignKey(to='gidb.Representation2D'),
         ),
         migrations.AddField(
             model_name='giinstance',
@@ -119,26 +159,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='gielement',
-            name='soil_type',
-            field=models.ForeignKey(to='gidb.RHESSSysSoilType', null=True),
-        ),
-        migrations.AddField(
-            model_name='gielement',
-            name='stratum_type',
-            field=models.ForeignKey(to='gidb.RHESSysStratumType', null=True),
-        ),
-        migrations.AddField(
-            model_name='gitemplate',
-            name='model_3d',
-            field=models.ForeignKey(to='gidb.Representation3D'),
-        ),
-        migrations.AddField(
-            model_name='gitemplate',
-            name='model_planview',
-            field=models.ForeignKey(to='gidb.Representation2D'),
-        ),
-        migrations.AddField(
-            model_name='gielement',
             name='model_3d',
             field=models.ForeignKey(to='gidb.Representation3D', null=True),
         ),
@@ -146,5 +166,15 @@ class Migration(migrations.Migration):
             model_name='gielement',
             name='model_planview',
             field=models.ForeignKey(to='gidb.Representation2D', null=True),
+        ),
+        migrations.AddField(
+            model_name='gielement',
+            name='soil_type',
+            field=models.ForeignKey(to='gidb.RHESSysSoilType', null=True),
+        ),
+        migrations.AddField(
+            model_name='gielement',
+            name='stratum_type',
+            field=models.ForeignKey(to='gidb.RHESSysStratumType', null=True),
         ),
     ]
