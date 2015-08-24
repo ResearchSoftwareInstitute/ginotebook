0c6d65e61774171a6f92761030a2b51c3e11d547
Brian Miles <selimnairb@gmail.com>
2015-08-24 15:01:12 -0400

    Fix view imports

--- gidb/urls.py
+++ gidb/urls.py
@@ -1,17 +1,17 @@
 from rest_framework.routers import DefaultRouter
 
-from . import views
+from views import rest
 
 router = DefaultRouter()
-router.register(r'watersheds', views.rest.WatershedViewSet, 'watershed')
-router.register(r'watersheds_boundaries', views.rest.WatershedBoundaryViewSet, 'watershedboundary')
-router.register(r'gi_instances', views.rest.GIInstanceViewSet, 'giinstance')
-router.register(r'human_pref_images', views.rest.HumanPrefImageViewSet, 'humanprefimage')
-router.register(r'gi_veg_growth_states', views.rest.GIVegGrowthStateViewSet, 'giveggrowthstate')
-router.register(r'templates_for_ecoclimates', views.rest.TemplatesForEcoClimateViewSet, 'templatesforecoclimate')
-router.register(r'gi_templates', views.rest.GITemplateViewSet, 'gitemplate')
-router.register(r'gi_elements', views.rest.GIElementViewSet, 'gielement')
-router.register(r'rhessys_stratum_types', views.rest.RHESSysStratumTypeViewSet, 'rhessysstratumtype')
-router.register(r'rhessys_soil_types', views.rest.RHESSysSoilTypeViewSet, 'rhessyssoiltype')
-router.register(r'representation_2d', views.rest.Representation2DViewSet, 'representation2d')
-router.register(r'representation_3d', views.rest.Representation3DViewSet, 'representation3d')
+router.register(r'watersheds', rest.WatershedViewSet, 'watershed')
+router.register(r'watersheds_boundaries', rest.WatershedBoundaryViewSet, 'watershedboundary')
+router.register(r'gi_instances', rest.GIInstanceViewSet, 'giinstance')
+router.register(r'human_pref_images', rest.HumanPrefImageViewSet, 'humanprefimage')
+router.register(r'gi_veg_growth_states', rest.GIVegGrowthStateViewSet, 'giveggrowthstate')
+router.register(r'templates_for_ecoclimates', rest.TemplatesForEcoClimateViewSet, 'templatesforecoclimate')
+router.register(r'gi_templates', rest.GITemplateViewSet, 'gitemplate')
+router.register(r'gi_elements', rest.GIElementViewSet, 'gielement')
+router.register(r'rhessys_stratum_types', rest.RHESSysStratumTypeViewSet, 'rhessysstratumtype')
+router.register(r'rhessys_soil_types', rest.RHESSysSoilTypeViewSet, 'rhessyssoiltype')
+router.register(r'representation_2d', rest.Representation2DViewSet, 'representation2d')
+router.register(r'representation_3d', rest.Representation3DViewSet, 'representation3d')
