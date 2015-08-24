554e69355fccc6cc0636537299feccc013a4cb19
Brian Miles <selimnairb@gmail.com>
2015-08-24 13:41:08 -0400

    Add watershed boundary model to REST API Re-factor REST API view class package

--- gidb/serializers.py
+++ gidb/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Watershed
+from .models import WatershedBoundary
 from .models import GIInstance
 from .models import HumanPrefImage
 from .models import GIVegGrowthState
@@ -13,11 +14,19 @@ from .models import Representation2D
 from .models import Representation3D
 
 
+class WatershedBoundarySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = WatershedBoundary
+        fields = ('url', 'id', 'name', 'boundary')
+
+
 class WatershedSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Watershed
-        fields = ('url', 'id', 'name', 'model_url', 'template_menu')
+        fields = ('url', 'id', 'name', 'model_url', 'template_menu', 'boundary')
+
 
 class GIInstanceSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -25,11 +34,13 @@ class GIInstanceSerializer(serializers.HyperlinkedModelSerializer):
         model = GIInstance
         fields = ('url', 'id', 'watershed', 'template', 'placement_poly')
 
+
 class HumanPrefImageSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = HumanPrefImage
         fields = ('url', 'id', 'name', 'gi_instance', 'pref_image')
+
 
 class GIVegGrowthStateSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -37,17 +48,20 @@ class GIVegGrowthStateSerializer(serializers.HyperlinkedModelSerializer):
         model = GIVegGrowthState
         fields = ('url', 'id', 'gi_instance', 'gi_element', 'leaf_area_index', 'stem_density', 'height')
 
+
 class TemplatesForEcoClimateSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = TemplatesForEcoClimate
         fields = ('url', 'id', 'name', 'templates')
 
+
 class GITemplateSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = GITemplate
         fields = ('url', 'id', 'name', 'model_3d', 'model_planview', 'gi_elements')
+
 
 class GIElementSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -56,11 +70,13 @@ class GIElementSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'id', 'name', 'model_3d', 'model_planview',
                   'stratum_type', 'soil_type', 'major_axis', 'minor_axis')
 
+
 class RHESSysStratumTypeSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = RHESSysStratumType
         fields = ('url', 'id', 'name', 'rhessys_default_id')
+
 
 class RHESSysSoilTypeSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -68,11 +84,13 @@ class RHESSysSoilTypeSerializer(serializers.HyperlinkedModelSerializer):
         model = RHESSysSoilType
         fields = ('url', 'id', 'name', 'rhessys_default_id')
 
+
 class Representation2DSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Representation2D
         fields = ('url', 'id', 'rep_file', 'rep_thumbnail')
+
 
 class Representation3DSerializer(serializers.HyperlinkedModelSerializer):
 
