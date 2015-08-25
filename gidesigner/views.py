3f2af5343f1e3841e9a71e681ec096b2faad5303
Brian Miles <selimnairb@gmail.com>
2015-08-25 13:53:06 -0400

    Add main view for GI Designer

--- gidesigner/views.py
+++ gidesigner/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from gidb.models import Watershed
 
 def index(request):
     context = {'watersheds_url': '/api/watersheds/'}
     return render(request, 'gidesigner/index.html', context)
 
 def main(request):
-    watershed = request.POST['watershed']
-    return HttpResponse("Watershed {0} was selected.".format(watershed))
+    watershed_id = request.POST['watershed']
+    watershed_name = Watershed.objects.get(pk=watershed_id)
+    context = {'watershed_id': watershed_id,
+               'watershed_name': watershed_name }
+    return render(request, 'gidesigner/main.html', context)
