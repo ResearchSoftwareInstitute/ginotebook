e32d808deda6d368034692fb080bcc647bcd0294
Brian Miles <selimnairb@gmail.com>
2015-08-25 12:32:53 -0400

    Add OpenLayers map to watershed chooser

--- gidesigner/views.py
+++ gidesigner/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 
 
 def index(request):
     context = {'watersheds_url': '/api/watersheds/'}
     return render(request, 'gidesigner/index.html', context)
+
+def main(request):
+    watershed = request.POST['watershed']
+    return HttpResponse("Watershed {0} was selected.".format(watershed))
